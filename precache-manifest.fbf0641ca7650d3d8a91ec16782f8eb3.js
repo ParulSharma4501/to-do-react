88b8a42c51f3e4d1c1c1e103df2e18f2acec0cfd
Parul Sharma <parul4501@gmail.com>
2021-01-06 18:58:35 +0530

    Updates

---
+++ precache-manifest.fbf0641ca7650d3d8a91ec16782f8eb3.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d6bfe2ae49cafd445c4787bd186b4cf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3cfe72f6f7927756de39",
+    "url": "/static/js/2.c04813ce.chunk.js"
+  },
+  {
+    "revision": "8c5a1d0f6c67f3f8f8ad",
+    "url": "/static/js/main.6d3a0715.chunk.js"
+  },
+  {
+    "revision": "db65393590a7b75d6022",
+    "url": "/static/js/runtime-main.f8e38478.js"
+  }
+]);
